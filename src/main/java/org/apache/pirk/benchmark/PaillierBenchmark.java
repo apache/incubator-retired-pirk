0afc8836907f9dba8a1f79aea89d4896797cf8c9
Jacob <git@jacobwilder.org>
2016-07-20 15:49:20 -0400

    This looks approximately correct and it compiles successfully. Haven't figured out how to run it yet though...

--- src/main/java/org/apache/pirk/benchmark/PaillierBenchmark.java
+++ src/main/java/org/apache/pirk/benchmark/PaillierBenchmark.java
@@ -1,23 +1,27 @@
 /*******************************************************************************
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *******************************************************************************/
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*******************************************************************************/
 
 package org.apache.pirk.benchmark;
+
+import com.google.caliper.BeforeExperiment;
+import com.google.caliper.Benchmark;
+import com.google.caliper.Param;
 
 import java.math.BigInteger;
 
@@ -27,56 +31,44 @@ import org.apache.pirk.encryption.Paillier;
 import org.apache.pirk.utils.LogUtils;
 import org.apache.pirk.utils.PIRException;
 import org.apache.pirk.utils.SystemConfiguration;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
+
 
 /**
- * A JMH benchmark to evaluate Paillier performance both with and without using com.square.jnagmp.gmp to accelerate modPow
- * <p>
- * Guides to using JMH can be found at: http://tutorials.jenkov.com/java-performance/jmh.html and http://nitschinger.at/Using-JMH-for-Java-Microbenchmarking/
- */
+* The purpose of these Pallier Benchmarks is to compare the performance of
+* Mod Pow using BigInteger's implementation and gmp's via Square's JNI connector.
+* 
+* mvn exec:java Dexec.mainClass="com.google.caliper.runner.CaliperMain" Dexec.args="org.apache.pirk.benchmark.PaillierBenchmark"
+*/
+public class PaillierBenchmark  {
 
-public class PaillierBenchmark
-{
-  private static final int MODULUS_SIZE = 3074;
+  private static BigInteger r1, m1;
+  private static Paillier paillier;
   private static Logger logger = LogUtils.getLoggerForThisClass();
+  private static final int MODULUS_SIZE = 3074;
 
-  @State(Scope.Benchmark)
-  public static class PaillierBenchmarkState
+  static
   {
-    BigInteger r1 = null; // random number in (Z/NZ)*
-    BigInteger m1 = null; // message to encrypt
-
-    Paillier pallier = null;
-
-    /**
-     * This sets up the state for the two separate benchmarks
-     */
-    @Setup(org.openjdk.jmh.annotations.Level.Trial)
-    public void setUp()
+    int systemPrimeCertainty = Integer.parseInt(SystemConfiguration.getProperty("pir.primeCertainty", "100"));
+    try
     {
-      int systemPrimeCertainty = Integer.parseInt(SystemConfiguration.getProperty("pir.primeCertainty", "100"));
-      try
-      {
-        pallier = new Paillier(MODULUS_SIZE, systemPrimeCertainty);
+      paillier = new Paillier(MODULUS_SIZE, systemPrimeCertainty);
 
-      } catch (PIRException e)
-      {
-        System.out.printf("Couldn't build pallier object!\n");
-      }
-
-      r1 = BigInteger.valueOf(3);
-      m1 = BigInteger.valueOf(5);
+    } catch (PIRException e)
+    {
+      System.out.printf("Couldn't build paillier object!\n");
     }
+
+    r1 = BigInteger.valueOf(3);
+    m1 = BigInteger.valueOf(5);
   }
 
+  // public static void main(String[] args) throws Exception {
+  //   new Runner().run(SetBenchmark.class.getName());
+  // }
+
+
   @Benchmark
-  @BenchmarkMode(Mode.Throughput)
-  public void testWithGMP(PaillierBenchmarkState allState)
+  public void timePallierWithGMP(int reps)
   {
     SystemConfiguration.setProperty("paillier.useGMPForModPow", "true");
     SystemConfiguration.setProperty("paillier.GMPConstantTimeMode", "false");
@@ -84,16 +76,19 @@ public class PaillierBenchmark
 
     try
     {
-      allState.pallier.encrypt(allState.m1, allState.r1);
+      for (int i = 0; i < reps; i++)
+      {
+        paillier.encrypt(m1, r1);
+      }
     } catch (PIRException e)
     {
       logger.info("Exception in testWithGMP!\n");
     }
   }
 
+
   @Benchmark
-  @BenchmarkMode(Mode.Throughput)
-  public void testWithGMPConstantTime(PaillierBenchmarkState allState)
+  public void timePallierWithGMPConstantTime(int reps)
   {
     SystemConfiguration.setProperty("paillier.useGMPForModPow", "true");
     SystemConfiguration.setProperty("paillier.GMPConstantTimeMode", "true");
@@ -101,26 +96,34 @@ public class PaillierBenchmark
 
     try
     {
-      allState.pallier.encrypt(allState.m1, allState.r1);
+      for (int i = 0; i < reps; i++)
+      {
+        paillier.encrypt(m1, r1);
+      }
     } catch (PIRException e)
     {
-      logger.info("Exception in testWithGMPConstantTime!\n");
+      logger.info("Exception in testWithGMP!\n");
     }
   }
 
+
   @Benchmark
-  @BenchmarkMode(Mode.Throughput)
-  public void testWithoutGMP(PaillierBenchmarkState allState)
+  public void timePallierWithoutGMP(int reps)
   {
     SystemConfiguration.setProperty("paillier.useGMPForModPow", "false");
+    SystemConfiguration.setProperty("paillier.GMPConstantTimeMode", "false");
     ModPowAbstraction.reloadConfiguration();
 
     try
     {
-      allState.pallier.encrypt(allState.m1, allState.r1);
+      for (int i = 0; i < reps; i++)
+      {
+        paillier.encrypt(m1, r1);
+      }
     } catch (PIRException e)
     {
-      logger.info("Exception in testWithoutGMP!\n");
+      logger.info("Exception in testWithGMP!\n");
     }
   }
+
 }
