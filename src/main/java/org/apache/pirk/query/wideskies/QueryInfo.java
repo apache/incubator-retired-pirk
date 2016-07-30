3262f0fdd52d62c050c6dc7fb6c6b41c34e99173
charris <charris@apache.org>
2016-07-30 15:41:55 -0400

    Added some functionality useful for Storm.

--- src/main/java/org/apache/pirk/query/wideskies/QueryInfo.java
+++ src/main/java/org/apache/pirk/query/wideskies/QueryInfo.java
@@ -39,28 +39,28 @@ public class QueryInfo implements Serializable
 
   private static Logger logger = LogUtils.getLoggerForThisClass();
 
-  double queryNum = 0.0; // the identifier num of the query
-  int numSelectors = 0; // the number of selectors in the query, given by \floor{paillerBitSize/dataPartitionBitSize}
+  private double queryNum = 0.0; // the identifier num of the query
+  private int numSelectors = 0; // the number of selectors in the query, given by \floor{paillerBitSize/dataPartitionBitSize}
 
-  String queryType = null; // QueryType string const
+  private String queryType; // QueryType string const
 
-  String queryName = null; // Name of query
+  private String queryName; // Name of query
 
-  int paillierBitSize = 0; // Paillier modulus size
+  private int paillierBitSize = 0; // Paillier modulus size
 
-  int hashBitSize = 0; // Bit size of the keyed hash function
-  String hashKey = null; // Key for the keyed hash function
+  private int hashBitSize = 0; // Bit size of the keyed hash function
+  private String hashKey; // Key for the keyed hash function
 
-  int numBitsPerDataElement = 0; // total num bits per returned data value - defined relative to query type
-  int dataPartitionBitSize = 0; // num of bits for each partition of an incoming data element, must be < 32 right now
-  int numPartitionsPerDataElement = 0; // num partitions of size dataPartitionBitSize per data element
+  private int numBitsPerDataElement = 0; // total num bits per returned data value - defined relative to query type
+  private int dataPartitionBitSize = 0; // num of bits for each partition of an incoming data element, must be < 32 right now
+  private int numPartitionsPerDataElement = 0; // num partitions of size dataPartitionBitSize per data element
 
-  boolean useExpLookupTable = false; // whether or not to generate and use the expLookupTable for encryption, it is very expensive to compute
+  private boolean useExpLookupTable = false; // whether or not to generate and use the expLookupTable for encryption, it is very expensive to compute
 
-  boolean useHDFSExpLookupTable = false; // whether or not to use the expLookupTable stored in HDFS
+  private boolean useHDFSExpLookupTable = false; // whether or not to use the expLookupTable stored in HDFS
   // if it doesn't yet exist, it will be created within the cluster and stored in HDFS
 
-  boolean embedSelector = true; // whether or not to embed the selector in the results - results in a very low
+  private boolean embedSelector = true; // whether or not to embed the selector in the results - results in a very low
 
   // false positive rate for variable length selectors and a zero false positive rate
   // for selectors of fixed size < 32 bits
@@ -105,19 +105,42 @@ public class QueryInfo implements Serializable
 
   public QueryInfo(Map queryInfoMap)
   {
-    queryNum = (double) queryInfoMap.get("queryNum");
-    queryType = (String) queryInfoMap.get("queryType");
-    queryName = (String) queryInfoMap.get("queryName");
-    numSelectors = ((Long) queryInfoMap.get("numSelectors")).intValue();
-    paillierBitSize = ((Long) queryInfoMap.get("paillierBitSize")).intValue();
-    hashBitSize = ((Long) queryInfoMap.get("hashBitSize")).intValue();
-    hashKey = (String) queryInfoMap.get("hashKey");
-    numBitsPerDataElement = ((Long) queryInfoMap.get("numBitsPerDataElement")).intValue();
-    numPartitionsPerDataElement = ((Long) queryInfoMap.get("numPartitionsPerDataElement")).intValue();
-    dataPartitionBitSize = ((Long) queryInfoMap.get("dataPartitionsBitSize")).intValue();
-    useExpLookupTable = (boolean) queryInfoMap.get("useExpLookupTable");
-    useHDFSExpLookupTable = (boolean) queryInfoMap.get("useHDFSExpLookupTable");
-    embedSelector = (boolean) queryInfoMap.get("embedSelector");
+    // Seemed that the Storm Config would serialize the map as a json and read back in with numeric values as longs.
+    // So had to cast as a long and call .intValue.  However, this didn't work in the PirkHashScheme and had to try
+    // the normal way of doing it as well.
+    try
+    {
+      queryNum = (double) queryInfoMap.get("queryNum");
+      queryType = (String) queryInfoMap.get("queryType");
+      queryName = (String) queryInfoMap.get("queryName");
+      numSelectors = ((Long) queryInfoMap.get("numSelectors")).intValue();
+      paillierBitSize = ((Long) queryInfoMap.get("paillierBitSize")).intValue();
+      hashBitSize = ((Long) queryInfoMap.get("hashBitSize")).intValue();
+      hashKey = (String) queryInfoMap.get("hashKey");
+      numBitsPerDataElement = ((Long) queryInfoMap.get("numBitsPerDataElement")).intValue();
+      numPartitionsPerDataElement = ((Long) queryInfoMap.get("numPartitionsPerDataElement")).intValue();
+      dataPartitionBitSize = ((Long) queryInfoMap.get("dataPartitionsBitSize")).intValue();
+      useExpLookupTable = (boolean) queryInfoMap.get("useExpLookupTable");
+      useHDFSExpLookupTable = (boolean) queryInfoMap.get("useHDFSExpLookupTable");
+      embedSelector = (boolean) queryInfoMap.get("embedSelector");
+    } catch (ClassCastException e)
+    {
+      queryNum = (double) queryInfoMap.get("queryNum");
+      queryType = (String) queryInfoMap.get("queryType");
+      queryName = (String) queryInfoMap.get("queryName");
+      numSelectors = (int) queryInfoMap.get("numSelectors");
+      paillierBitSize = (int) queryInfoMap.get("paillierBitSize");
+      hashBitSize = (int) queryInfoMap.get("hashBitSize");
+      hashKey = (String) queryInfoMap.get("hashKey");
+      numBitsPerDataElement = (int) queryInfoMap.get("numBitsPerDataElement");
+      numPartitionsPerDataElement = (int) queryInfoMap.get("numPartitionsPerDataElement");
+      dataPartitionBitSize = (int) queryInfoMap.get("dataPartitionsBitSize");
+      useExpLookupTable = (boolean) queryInfoMap.get("useExpLookupTable");
+      useHDFSExpLookupTable = (boolean) queryInfoMap.get("useHDFSExpLookupTable");
+      embedSelector = (boolean) queryInfoMap.get("embedSelector");
+
+    }
+
   }
 
   public double getQueryNum()
