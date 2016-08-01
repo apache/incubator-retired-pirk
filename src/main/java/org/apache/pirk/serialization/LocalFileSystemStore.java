5ebe1bf2fddbd0ff9e36016bc49afb4ba33ab09d
smarthi <smarthi@apache.org>
2016-08-01 19:43:08 -0400

    [PIRK-41]: Add JSON Serialization for Java Objects -- closes apache/incubator-pirk#42

--- src/main/java/org/apache/pirk/serialization/LocalFileSystemStore.java
+++ src/main/java/org/apache/pirk/serialization/LocalFileSystemStore.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *******************************************************************************/
+ */
 package org.apache.pirk.serialization;
 
 import java.io.File;
@@ -59,7 +59,7 @@ public class LocalFileSystemStore extends StorageService
   /**
    * Stores the given object at the given file location. The object is serialized using the configured serializer.
    * 
-   * @param path
+   * @param file
    *          The local file system location to store the object.
    * @param obj
    *          The object to store.
