9233b19e2efc08738ebbf230feb09d48a197e2cd
eawilliams <eawilliams@apache.org>
2016-08-11 10:04:48 -0400

    [PIRK-29]: User should be notified when attempting to decrypt with wrong querier - closes apache/incubator-pirk#55

--- src/main/java/org/apache/pirk/querier/wideskies/QuerierDriver.java
+++ src/main/java/org/apache/pirk/querier/wideskies/QuerierDriver.java
@@ -203,6 +203,15 @@ public class QuerierDriver implements Serializable
       Response response = storage.recall(inputFile, Response.class);
       Querier querier = storage.recall(querierFile, Querier.class);
 
+      UUID querierQueryID = querier.getQuery().getQueryInfo().getIdentifier();
+      UUID responseQueryID = response.getQueryInfo().getIdentifier();
+      if (!querierQueryID.equals(responseQueryID))
+      {
+        logger.error("The query identifier in the Response: " + responseQueryID.toString() + " does not match the query identifier specified in the Querier: "
+            + querierQueryID.toString());
+        System.exit(0);
+      }
+
       // Perform decryption and output the result file
       DecryptResponse decryptResponse = new DecryptResponse(response, querier);
       decryptResponse.decrypt(numThreads);
