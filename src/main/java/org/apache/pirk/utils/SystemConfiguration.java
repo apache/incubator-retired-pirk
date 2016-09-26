4ded20cd8b4198560f65a58b19b109773b8713c6
Walter Ray-Dulany <raydulany@gmail.com>
2016-09-26 08:17:00 -0700

    Ammend SystemConfiguration to reassert log4j settings.

--- src/main/java/org/apache/pirk/utils/SystemConfiguration.java
+++ src/main/java/org/apache/pirk/utils/SystemConfiguration.java
@@ -30,6 +30,7 @@ import java.util.Properties;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.pirk.utils.LogUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -58,6 +59,10 @@ public class SystemConfiguration
 
   private static final String LOCAL_PROPERTIES_DIR = "local.pirk.properties.dir";
 
+  // The below cannot be named "log4j.properties" as that name will seek to
+  // hadoop's similarly named file and not our own.
+  private static final String LOG_PROPERTIES_FILE = "mylog4j.properties";
+
   static
   {
     initialize();
@@ -76,6 +81,10 @@ public class SystemConfiguration
 
     // Try to load the local properties files, if they exists
     loadPropsFromDir(getProperty(LOCAL_PROPERTIES_DIR));
+
+    // Try to reassert our own log properties
+    // Needed to ensure that we can change log levels in hadoop/mapreduce.
+    LogUtils.reassertLogProperties();
   }
 
   /**
